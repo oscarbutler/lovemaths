03259793d40d34113692cef717f6d7e4281aee57
Oscar Butler <oscarbutler>
2023-11-13 20:25:03 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,12 +14,23 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
     }
+
+runGame("addition")
+
 });
 
-function runGame() {
+function runGame(gameType) {
     /*Two Random Numbers*/
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;  
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
+
 }
 
 function checkAnswer() {
@@ -38,8 +49,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion() {
