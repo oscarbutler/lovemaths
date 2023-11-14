341690c95cd67424bbc661db2092fbd260551d93
Oscar Butler <oscarbutler>
2023-11-14 11:31:09 +0000

    Add calculation for the correct answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,11 +34,30 @@ function runGame(gameType) {
 }
 
 function checkAnswer() {
+let userAnswer = parseInt(document.getElementById("answer-box").value);
+let calculatedAnswer = calculatedCorrectAnswer();
+let isCorrect = userAnswer === calculatedAnswer[0];
 
+if (isCorrect) {
+    alert("Hey! You got it right!");
+} else {
+    alert(`You answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`)
+
+}
+runGame(calculatedAnswer[1]);   
 }
 
 function calculateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById("operator").innerText;
 
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
+    }
 }
 
 function incrementScore() {
